a686e06b281de53df8baf48ff126557cfb2241a4
Mikkel Malmberg <mikkel@brnbw.com>
2017-05-10 13:17:44 +0200

    Add pino-http, index page

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const pino = require('pino')()
+const log = require('pino-http')({logger: pino})
 const http = require('http')
 const https = require('https')
 const fs = require('fs')
@@ -9,52 +10,61 @@ const path = require('path')
 
 const port = process.env.PORT || 3000
 
-http
-  .createServer((req, res) => {
-    pino.info(req)
+const server = http.createServer(handle)
 
-    const localPath = `/tmp${req.url}`
+server.listen(port, err => {
+  if (err) return pino.error(err)
+  pino.info(`Ready at :${port}`)
+})
 
-    fs.open(localPath, 'r', (err, fd) => {
-      if (err && err.code === 'ENOENT') {
-        pino.info('No local version present, fetching...')
+function handle (req, res) {
+  log(req, res)
 
-        const url = `https://s3.amazonaws.com${req.url}`
-        mkdirp.sync(path.dirname(localPath))
-        const localFile = fs.createWriteStream(localPath)
+  if (req.url === '/') {
+    res.write('ok')
+    res.end()
+    return
+  }
 
-        const get = https.get(url, proxyRes => {
-          res.writeHead(proxyRes.statusCode, proxyRes.headers)
-          proxyRes.pipe(res, {end: true})
-          proxyRes.pipe(localFile)
-        })
+  const localPath = `/tmp${req.url}`
 
-        get.on('error', err => {
-          pino.error(err)
-        })
+  fs.open(localPath, 'r', (err, fd) => {
+    if (err && err.code === 'ENOENT') {
+      pino.info('No local version present, fetching...')
 
-        get.on('end', () => {
-          pino.info('Done fetching!')
-        })
+      const url = `https://s3.amazonaws.com${req.url}`
+      mkdirp.sync(path.dirname(localPath))
+      const localFile = fs.createWriteStream(localPath)
 
-        return
-      }
-
-      if (err) throw err
-
-      pino.info('Cached file found, sending ...')
-
-      const readStream = fs.createReadStream(undefined, {fd: fd})
-      const stat = fs.statSync(localPath)
-
-      res.writeHead(200, {
-        'Content-Type': 'application/x-gzip',
-        'Content-Length': stat.size
+      const get = https.get(url, proxyRes => {
+        res.writeHead(proxyRes.statusCode, proxyRes.headers)
+        proxyRes.pipe(res, {end: true})
+        proxyRes.pipe(localFile)
       })
-      readStream.pipe(res, {end: true})
+
+      get.on('error', err => {
+        pino.error(err)
+      })
+
+      get.on('end', () => {
+        pino.info('Done fetching!')
+      })
+
+      return
+    }
+
+    if (err) throw err
+
+    pino.info('Cached file found, sending ...')
+
+    const readStream = fs.createReadStream(undefined, {fd: fd})
+    const stat = fs.statSync(localPath)
+
+    res.writeHead(200, {
+      'Content-Type': 'application/x-gzip',
+      'Content-Length': stat.size
     })
+
+    readStream.pipe(res, {end: true})
   })
-  .listen(port, err => {
-    if (err) throw new Error(err)
-    pino.info(`Ready at ${port}`)
-  })
+}
